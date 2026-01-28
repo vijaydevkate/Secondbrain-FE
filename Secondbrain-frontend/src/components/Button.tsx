73ee398c35851ccc5cca75d874db7566cd9abf34
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-28 11:19:59 -0500

    created interface for Buttonprops

--- Secondbrain-frontend/src/components/Button.tsx
+++ Secondbrain-frontend/src/components/Button.tsx
@@ -1,7 +1,10 @@
+import type { ReactElement } from "react"
 
 //props for the button
 interface Buttonprops{
-   
+    variant: "Primary" | "Secondary",
+    text: String,
+    startIcon? : ReactElement
 }
 export function Button(props:Buttonprops) {
     return (
