16a427d309b22195df32026cf9d4d6c73ff0a629
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-29 10:18:05 -0500

    Added secondary button

--- Secondbrain-frontend/src/App.tsx
+++ Secondbrain-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Button } from './components/Button'
+import { PlusIcon } from './Icons/PlusIcon'
 import { ShareIcon } from './Icons/ShareIcon'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Button variant="primary" text="Share" startIcon={<ShareIcon/>}></Button> 
+    <Button variant= "secondary" text="Add" startIcon={<PlusIcon/>}></Button>
     </>
   )
 }
