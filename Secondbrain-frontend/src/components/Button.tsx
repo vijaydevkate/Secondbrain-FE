38c63a8b7b5bc672ad32b1e76019e5553f6470ab
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-29 10:14:10 -0500

    Primary button created

--- Secondbrain-frontend/src/components/Button.tsx
+++ Secondbrain-frontend/src/components/Button.tsx
@@ -13,10 +13,14 @@ const variantsprops = {
     secondary:"bg-purple-200 text-purple-400"
 }
 
+const buttondefaultstyles = "px-4 py-2 rounded-md flex items-center"
+
 export function Button({variant, text, startIcon}:Buttonprops) {
     return (
-        <button className={variantsprops[variant]}>
+        <button className={`${variantsprops[variant]} ${buttondefaultstyles}`}>
+           <div className="pr-1">
             {startIcon}
+           </div>
             {text}
         </button>
     )
