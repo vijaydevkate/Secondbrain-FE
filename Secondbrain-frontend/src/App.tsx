33351147b787396b52ca6f988a3720c105f3b684
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-31 10:23:23 -0500

    structuring card and button component on screen

--- Secondbrain-frontend/src/App.tsx
+++ Secondbrain-frontend/src/App.tsx
@@ -9,9 +9,16 @@ function App() {
 
   return (
     <>
-    <Button variant="primary" text="Share Brain" startIcon={<ShareIcon/>}></Button> 
-    <Button variant= "secondary" text="Add content" startIcon={<PlusIcon/>}></Button>
-    <Card type= "youtube" link="https://www.youtube.com/watch?v=oGKIhfenTRk" title= "youtube link"/>
+    <div className='flex p-4 justify-end gap-4'>
+      <Button variant="primary" text="Share Brain" startIcon={<ShareIcon/>}>
+      </Button> 
+      <Button variant= "secondary" text="Add content" startIcon={<PlusIcon/>}></Button>
+    </div>
+
+    <div className='flex gap-4 p-4 flex-wrap'>
+      <Card type= "youtube" link="https://www.youtube.com/watch?v=WovgAKWSYY4" title= "youtube link"/>
+      <Card type= "youtube" link="https://www.youtube.com/watch?v=oGKIhfenTRk" title= "youtube link"/>
+    </div>
     </>
   )
 }
