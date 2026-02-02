5bf8bc1380e058c85eb37a22cd55c177b8e43d10
vijaydevkate <vijaydevkate98@gmail.com>
2026-02-02 11:48:17 -0500

    Added routes

--- Secondbrain-frontend/src/App.tsx
+++ Secondbrain-frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import { Dashboard } from './pages/Dashboard'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
        
        <Signin/>
 
