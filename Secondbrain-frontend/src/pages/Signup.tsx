578c5fbea09243ff2a3f2b6b6ce022ac3126ebb6
vijaydevkate <vijaydevkate98@gmail.com>
2026-02-06 14:00:00 -0500

    Installed axios and used backend URL

--- Secondbrain-frontend/src/pages/Signup.tsx
+++ Secondbrain-frontend/src/pages/Signup.tsx
@@ -1,15 +1,24 @@
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 import { useRef } from "react"
+import { BACKEND_URL } from "../Config"
+import axios from "axios"
 
 export function Signup(){
     const usernameref = useRef<HTMLInputElement>(null)
     const passwordref = useRef<HTMLInputElement>(null)
 
-    function signup(){
+    async function signup(){
         //Reading inputvalue input
         const username = usernameref.current?.value
         const password = passwordref.current?.value
+      await axios.post(BACKEND_URL + "api/v1/signup",{
+            data:{
+                username,
+                password
+            }
+        })
+        alert("You have signedup")
     }
      return(
         <>
